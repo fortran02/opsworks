d8195f3e2bfd6d72f305bbb83946f6f010e43825
Prem Rara <prem@cascadeo.com>
2014-06-19 18:03:53 +0800

    Fix recipe issues

--- custom-zookeeper/recipes/default.rb
+++ custom-zookeeper/recipes/default.rb
@@ -2,15 +2,18 @@
 #
 # Copyright 2014, Cascadeo Corp.
 
-zk_server_script = "#{node[:zookeeper][:install_dir]}/bin/zkServer.sh"
+zk_server_script = ::File.join(node[:zookeeper][:install_dir],
+                          "zookeeper-#{node[:zookeeper][:version]}",
+                          'bin',
+                          'zkServer.sh')
+
 template zk_server_script do
     source "zkServer.sh.erb"
     owner node[:zookeeper][:user]
     mode "0755"
-    variables (
+    variables ({
       :transaction_dir => node[:exhibitor][:transaction_dir],
       :jvm_min_heap_mem => node[:zookeeper][:jvm_min_heap_mem],
       :jvm_max_heap_mem => node[:zookeeper][:jvm_max_heap_mem]
-    )
+    })
 end
-
