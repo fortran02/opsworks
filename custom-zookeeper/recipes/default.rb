888c05920ae8086ea8005a2a93bfe032e9e04a8c
Prem Rara <prem@cascadeo.com>
2014-06-19 13:29:04 +0800

    Adding cookbook for customizing zookeeper installation

---
+++ custom-zookeeper/recipes/default.rb
@@ -0,0 +1,16 @@
+# recipes/default.rb
+#
+# Copyright 2014, Cascadeo Corp.
+
+zk_server_script = "#{node[:zookeeper][:install_dir]}/bin/zkServer.sh"
+template zk_server_script do
+    source "zkServer.sh.erb"
+    owner node[:zookeeper][:user]
+    mode "0755"
+    variables (
+      :transaction_dir => node[:exhibitor][:transaction_dir],
+      :jvm_min_heap_mem => node[:zookeeper][:jvm_min_heap_mem],
+      :jvm_max_heap_mem => node[:zookeeper][:jvm_max_heap_mem]
+    )
+end
+
