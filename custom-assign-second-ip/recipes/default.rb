8a6fe0269931582a77cbc04f4b0a063eb4134a0b
Prem Rara <prem@cascadeo.com>
2014-07-08 23:51:02 +0800

    add verification check to added secondary IP
    Fix to verify secondary IP assignment
    
    ruby block for get instance aws info
    
    implement in libraries
    
    fixes

--- custom-assign-second-ip/recipes/default.rb
+++ custom-assign-second-ip/recipes/default.rb
@@ -1,32 +1,31 @@
 include_recipe "custom-aws-cli-creds::default"
 
 require "json"
+require "open-uri"
+
+Chef::Resource::RubyBlock.send(:include, HelperFunctions)
 
 ENV['AWS_CONFIG_FILE'] = ::File.join(node['custom-aws-cli-creds']['config-dir'], "aws_cli_config")
+
+ruby_block "verify_secondary_IP" do
+  block do
+    raise "Cannot find secondary IP!" unless secondary_ip_exists()
+    Chef::Log.info("Secondary IP #{@secondary_private_ip} was assigned.")
+  end
+  action :nothing
+end
 
 service "network" do
   service_name "network"
   action :nothing
 end
 
-subnet_id = node[:opsworks][:instance][:subnet_id]
-Chef::Log.info("SUBNET ID: " + subnet_id)
-private_ip = node[:opsworks][:instance][:private_ip]
-Chef::Log.info("Private IP: " + private_ip)
-interfaces = `aws ec2 describe-network-interfaces \
-              --filters Name=private-ip-address,Values=#{private_ip}`
-parsed = JSON.parse(interfaces)
-eni_id = parsed["NetworkInterfaces"][0]["NetworkInterfaceId"]
-Chef::Log.info("ENI ID: " + eni_id)
-
-secondary_private_ip = node["custom-assign-second-ip"][subnet_id]
-
-execute "second_ip_add" do
-  command "aws ec2 assign-private-ip-addresses \
-    --network-interface-id #{eni_id} \
-    --private-ip-addresses #{secondary_private_ip} \
-    --allow-reassignment
-  "
-  notifies :restart, "service[network]", :delayed
+ruby_block "assign_secondary_IP" do
+  not_if { secondary_ip_exists() }
+  block do
+    assign_secondary_ip()
+  end
+  notifies :restart, "service[network]", :immediately
+  notifies :create, "ruby_block[verify_secondary_IP]", :immediately
 end
 
