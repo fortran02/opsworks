badce71a6ca8ebb57f305725f9e420f59b197e1b
Prem Rara <prem@cascadeo.com>
2014-07-08 05:05:13 +0000

    Fixes to second IP cookbook

--- custom-assign-second-ip/recipes/default.rb
+++ custom-assign-second-ip/recipes/default.rb
@@ -1,19 +1,16 @@
 
 require "json"
 
-directory "~/.aws/" do
-  mode "0600"
-end
-
-template "~/.aws/config" do
-  source "config.erb"
-  mode "0600"
-  variables(
-    :ec2region  => node[:second-ip][:ec2region],
-    :ec2key     => node[:second-ip][:ec2key],
-    :ec2secret  => node[:second-ip][:ec2secret]
-  )
-end
+ENV['AWS_CONFIG_FILE'] = "/root/.aws/config"
+#template "#{ENV['AWS_CONFIG_FILE']}" do
+#  source "config.erb"
+#  mode "0600"
+#  variables(
+#    :ec2region  => node["second-ip"][:ec2region],
+#    :ec2key     => node["second-ip"][:ec2key],
+#    :ec2secret  => node["second-ip"][:ec2secret]
+#  )
+#end
 
 service "network" do
   service_name "network"
@@ -30,12 +27,12 @@ parsed = JSON.parse(interfaces)
 eni_id = parsed["NetworkInterfaces"][0]["NetworkInterfaceId"]
 Chef::Log.info("ENI ID: " + eni_id)
 
-new_private_ip = node[:second_ip][subnet_id][:ip]
+new_private_ip = node["second-ip"][subnet_id]
 
 execute "second_ip_add" do
-  command "aws ec2 assign-private-ip-addresses
-    --network-interface-id #{eni_id}
-    --private-ip-addresses #{new_private_ip}
+  command "aws ec2 assign-private-ip-addresses \
+    --network-interface-id #{eni_id} \
+    --private-ip-addresses #{new_private_ip} \
     --allow-reassignment
   "
   notifies :restart, "service[network]", :delayed
