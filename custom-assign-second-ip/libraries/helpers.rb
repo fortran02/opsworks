e91a090b13446fc6255b1c57ee809e6c173193f0
Prem Rara <prem@cascadeo.com>
2014-07-09 12:26:25 +0800

    implement in libraries

---
+++ custom-assign-second-ip/libraries/helpers.rb
@@ -0,0 +1,36 @@
+module HelperFunctions
+
+  subnet_id = node[:opsworks][:instance][:subnet_id]
+  secondary_private_ip = node["custom-assign-second-ip"][subnet_id]
+
+  def secondary_ip_exists()
+    Chef::Log.info("Checking for secondary IP")
+    eth0_mac = `/sbin/ifconfig eth0 | awk '/HWaddr/ {print $5}'`.strip.downcase
+    Chef::Log.info("eth0 MAC: #{eth0_mac}")
+    check_url = "http://169.254.169.254/latest/meta-data/network/interfaces/macs/#{eth0_mac}/local-ipv4s"
+    attached_ips = open(check_url).read
+    attached_ips.split("\n").include?(secondary_private_ip)
+  end
+
+  def get_eni_id()
+    private_ip = node[:opsworks][:instance][:private_ip]
+    Chef::Log.info("Getting ENI_ID for Private IP: " + private_ip)
+    interfaces = `aws ec2 describe-network-interfaces \
+              --filters Name=private-ip-address,Values=#{private_ip}`
+    parsed = JSON.parse(interfaces)
+    eni_id = parsed["NetworkInterfaces"][0]["NetworkInterfaceId"]
+    Chef::Log.info("ENI ID: " + eni_id)
+    eni_id
+  end
+
+  def assign_secondary_ip()
+    Chef::Log.info("Assigning Secondary IP: #{secondary_private_ip}")
+    eni_id = get_eni_id()
+    assign = `aws ec2 assign-private-ip-addresses \
+            --network-interface-id #{eni_id} \
+            --private-ip-addresses #{secondary_private_ip} \
+            --allow-reassignment`
+    Chef::Log.info("IP assignment output:" + assign)
+  end
+end
+
--- custom-assign-second-ip/recipes/default.rb
+++ custom-assign-second-ip/recipes/default.rb
@@ -1,44 +1,34 @@
 include_recipe "custom-aws-cli-creds::default"
 
+class Chef::Recipe
+  include HelperFunctions
+end
+
 require "json"
 require "open-uri"
 
 ENV['AWS_CONFIG_FILE'] = ::File.join(node['custom-aws-cli-creds']['config-dir'], "aws_cli_config")
-subnet_id = node[:opsworks][:instance][:subnet_id]
 secondary_private_ip = node["custom-assign-second-ip"][subnet_id]
 
-ruby_block "get instance aws info" do
+ruby_block "verify_secondary_IP" do
   block do
-    Chef::Log.info("SUBNET ID: " + subnet_id)
-    private_ip = node[:opsworks][:instance][:private_ip]
-    Chef::Log.info("Private IP: " + private_ip)
-    interfaces = `aws ec2 describe-network-interfaces \
-              --filters Name=private-ip-address,Values=#{private_ip}`
-    parsed = JSON.parse(interfaces)
-    eni_id = parsed["NetworkInterfaces"][0]["NetworkInterfaceId"]
-    Chef::Log.info("ENI ID: " + eni_id)
-    Chef::Log.info("Secondary IP: #{secondary_private_ip}")
-    assign = `aws ec2 assign-private-ip-addresses \
-            --network-interface-id #{eni_id} \
-            --private-ip-addresses #{secondary_private_ip} \
-            --allow-reassignment`
-    Chef::Log.info("IP assignment output:" + assign)
+    raise "Cannot find secondary IP!" unless secondary_ip_exists()
+    Chef::Log.info("Secondary IP #{secondary_private_ip} was assigned.")
   end
+  action :nothing
 end
 
 service "network" do
   service_name "network"
-  action :restart
+  action :nothing
 end
 
-ruby_block "verify secondary IP" do
+ruby_block "assign_secondary_IP" do
+  not_if { secondary_ip_exists() }
   block do
-    eth0_mac = `/sbin/ifconfig eth0 | awk '/HWaddr/ {print $5}'`.strip.downcase
-    Chef::Log.info("eth0 MAC: #{eth0_mac}")
-    check_url = "http://169.254.169.254/latest/meta-data/network/interfaces/macs/#{eth0_mac}/local-ipv4s"
-    attached_ips = open(check_url).read
-    raise "Cannot find secondary IP!" unless attached_ips.split("\n").include?(secondary_private_ip)
-    Chef::Log.info("Secondary IP #{secondary_private_ip} was assigned.")
+    assign_secondary_ip()
   end
+  notifies :restart, "service[network]", :immediately
+  notifies :create, "ruby_block[verify_secondary_IP]", :immediately
 end
 
